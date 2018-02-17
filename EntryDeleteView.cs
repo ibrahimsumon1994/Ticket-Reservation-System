b42a17f2c9f91decc6b4c873c130ba8fc5ef9d1b
Ibrahim Sumon <sumonhtc45@gmail.com>
2018-02-18 00:30:44 +0600

    testing1

---
+++ EntryDeleteView.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class EntryDeleteView : Form
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=SUMON-PC\SQLEXPRESS;Initial Catalog=Sumon;Integrated Security= true;");
+        public EntryDeleteView()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        //previous(Log In page)
+        private void button5_Click(object sender, EventArgs e)
+        {
+            AdminLogIn obj = new AdminLogIn();
+
+            obj.Show();
+            this.Hide();
+        }
+
+        //Entry new bus
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox8.Text != "" && textBox5.Text != "" && textBox1.Text != "" && textBox2.Text != "" && textBox9.Text != "" && textBox3.Text != "" && textBox4.Text != "" && textBox6.Text != "" && textBox7.Text != "")
+            {
+                conn.Open();
+                string query = "INSERT INTO new_bus_info (bus_id,bus_name,from_where,to_where,date_of_journey,dep_time,arr_time,avai_seat,fare)VALUES (@bus_id,@bus_name,@from_where,@to_where,@date_of_journey,@dep_time,@arr_time,@avai_seat,@fare) ";
+                SqlCommand bcmd = new SqlCommand(query, conn);
+                bcmd.Parameters.AddWithValue("@bus_id", Convert.ToInt32(textBox8.Text));
+                bcmd.Parameters.AddWithValue("@bus_name", textBox5.Text);
+                bcmd.Parameters.AddWithValue("@from_where", textBox1.Text);
+                bcmd.Parameters.AddWithValue("@to_where", textBox2.Text);
+                bcmd.Parameters.AddWithValue("@date_of_journey", textBox9.Text);
+                bcmd.Parameters.AddWithValue("@dep_time", textBox3.Text);
+                bcmd.Parameters.AddWithValue("@arr_time", textBox4.Text);
+                bcmd.Parameters.AddWithValue("@avai_seat", Convert.ToInt32(textBox6.Text));
+                bcmd.Parameters.AddWithValue("@fare", Convert.ToInt32(textBox7.Text));
+                bcmd.ExecuteNonQuery();
+                MessageBox.Show("Information Inserted Successfully");
+                conn.Close();
+                LoadData();
+                ClearData();  
+            }
+            else
+            {
+                MessageBox.Show("Please Provide Details!");
+            } 
+        }
+
+        //Customer details
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CustomerDetails obj = new CustomerDetails();
+
+            obj.Show();
+            this.Hide();
+        }
+
+        //Delete bus schedule
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox8.Text != "")
+            {
+                conn.Open();
+                string query = "delete from new_bus_info where bus_id='" + textBox8.Text + "'";
+                SqlDataAdapter sda = new SqlDataAdapter(query, conn);
+                sda.SelectCommand.ExecuteNonQuery();
+                MessageBox.Show("Delete successfully");
+                conn.Close();
+                LoadData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please Select Record to Delete");
+            } 
+        }
+
+        //Update bus schedule
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox8.Text != "" && textBox5.Text != "" && textBox1.Text != "" && textBox2.Text != "" && textBox9.Text != "" && textBox3.Text != "" && textBox4.Text != "" && textBox6.Text != "" && textBox7.Text != "")
+            {
+                conn.Open();
+                string query = "update new_bus_info set bus_id = '" + textBox8.Text + "', bus_name = '" + textBox5.Text + "', from_where = '" + textBox1.Text + "', to_where = '" + textBox2.Text + "', dep_time = '" + textBox3.Text + "', arr_time = '" + textBox4.Text + "', avai_seat = '" + textBox6.Text + "', fare = '" + textBox7.Text + "', date_of_journey = '" + textBox9.Text + "'  where bus_id = '" + textBox8.Text + "'";
+                SqlCommand bcmd = new SqlCommand(query, conn);
+                bcmd.Parameters.AddWithValue("@bus_id", Convert.ToInt32(textBox8.Text));
+                bcmd.Parameters.AddWithValue("@bus_name", textBox5.Text);
+                bcmd.Parameters.AddWithValue("@from_where", textBox1.Text);
+                bcmd.Parameters.AddWithValue("@to_where", textBox2.Text);
+                bcmd.Parameters.AddWithValue("@date_of_journey", textBox9.Text);
+                bcmd.Parameters.AddWithValue("@dep_time", textBox3.Text);
+                bcmd.Parameters.AddWithValue("@arr_time", textBox4.Text);
+                bcmd.Parameters.AddWithValue("@avai_seat", Convert.ToInt32(textBox6.Text));
+                bcmd.Parameters.AddWithValue("@fare", Convert.ToInt32(textBox7.Text));
+                bcmd.ExecuteNonQuery();
+                MessageBox.Show("Updated successfully");
+                conn.Close();
+                LoadData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please Select Record To Update");
+            } 
+        }
+
+        private void LoadData()
+        {
+            string cmd = "select * from new_bus_info";
+            SqlDataAdapter adp = new SqlDataAdapter(cmd, conn);
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            dataGridView1.DataSource = dt;
+        } 
+
+        private void ClearData()
+        {
+            textBox8.Text = "";
+            textBox5.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox9.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            textBox8.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            textBox5.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+            textBox4.Text = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+            textBox6.Text = dataGridView1.Rows[e.RowIndex].Cells[6].Value.ToString();
+            textBox7.Text = dataGridView1.Rows[e.RowIndex].Cells[7].Value.ToString();
+            textBox9.Text = dataGridView1.Rows[e.RowIndex].Cells[8].Value.ToString();
+        } 
+    }
+}
---
+++ Welcome.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Welcome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Image = global::WindowsFormsApplication1.Properties.Resources.fondo_azul;
+            this.label1.Location = new System.Drawing.Point(424, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(449, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bus Reservation System";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(620, 232);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(732, 463);
+            this.panel2.TabIndex = 0;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(445, 295);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(78, 31);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Next";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.Location = new System.Drawing.Point(187, 253);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(222, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "To continue click next.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(185, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(338, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Welcome To The System";
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.online_reservation_of_bus_3_7282;
+            this.panel1.Location = new System.Drawing.Point(0, 232);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(620, 463);
+            this.panel1.TabIndex = 1;
+            // 
+            // Welcome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.fondo_azul;
+            this.ClientSize = new System.Drawing.Size(1350, 730);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Welcome";
+            this.Text = "Welcome";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+    }
+}
